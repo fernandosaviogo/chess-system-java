227c8e88751a1de53aee9e0255e98dab25543ff7
Fernando Sávio Gomes <133050259+fernandosaviogo@users.noreply.github.com>
2025-05-09 17:48:20 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,35 @@
+package boardgame;
+
+public class Position {
+	
+	private int row;
+	private int column;
+	
+	// Metodo construtor
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	// Metodos Gets e Sets
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+}
